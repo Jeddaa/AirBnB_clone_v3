030ea1eef1efab8a84196b5b10cf9f56bead22d6
jeddaa <fathiato@gmail.com>
2022-11-22 02:07:25 +0100

    API

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -4,7 +4,7 @@ from models.place import Place
 from models.amenity import Amenity
 from models import storage
 from api.v1.views import app_views
-from os import environ
+from os import getenv
 from flask import abort, jsonify, make_response, request
 from flasgger.utils import swag_from
 
@@ -64,8 +64,6 @@ def delete_place_amenity(place_id, amenity_id):
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'],
                  strict_slashes=False)
-@swag_from('documentation/place_amenity/post_place_amenities.yml',
-           methods=['POST'])
 def post_place_amenity(place_id, amenity_id):
     """
     Link a Amenity object to a Place
@@ -79,17 +77,15 @@ def post_place_amenity(place_id, amenity_id):
 
     if not amenity:
         abort(404)
-
-    if environ.get('HBNB_TYPE_STORAGE') == "db":
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         if amenity in place.amenities:
             return make_response(jsonify(amenity.to_dict()), 200)
-        else:
-            place.amenities.append(amenity)
+        place.amenities.append(amenity)
     else:
         if amenity_id in place.amenity_ids:
             return make_response(jsonify(amenity.to_dict()), 200)
-        else:
-            place.amenity_ids.append(amenity_id)
+        place.amenity_ids.append(amenity_id)
 
     storage.save()
     return make_response(jsonify(amenity.to_dict()), 201)
+

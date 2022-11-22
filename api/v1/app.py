8fbdd77aa7b67ae32d67d2a89e440caa97e76b65
jeddaa <fathiato@gmail.com>
2022-11-22 01:10:19 +0100

    API

--- api/v1/app.py
+++ api/v1/app.py
@@ -12,6 +12,25 @@ app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+app.config['SWAGGER'] = {
+    "swagger_version": "2.0",
+    "title": "Flasgger",
+    "headers": [
+        ('Access-Control-Allow-Origin', '*'),
+        ('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, OPTIONS"),
+        ('Access-Control-Allow-Credentials', "true"),
+    ],
+    "specs": [
+        {
+            "version": "1.0",
+            "title": "HBNB API",
+            "endpoint": 'v1_views',
+            "description": 'HBNB REST API',
+            "route": '/v1/views',
+        }
+    ]
+}
+swagger = Swagger(app)
 
 
 @app.teardown_appcontext
@@ -25,13 +44,6 @@ def page_not_found(e):
     """ a handler for 404 errors that returns a
     JSON-formatted 404 status code response"""
     return jsonify({"error": "Not found"}), 404
-
-app.config['SWAGGER'] = {
-    'title': 'AirBnB clone Restful API',
-    'uiversion': 3
-}
-
-Swagger(app)
 
 
 if __name__ == "__main__":

d640af119b8b17e6d903023ab9d058b682ca56be
jeddaa <fathiato@gmail.com>
2022-11-22 02:00:32 +0100

    API

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -12,6 +12,7 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
+from api.v1.views.places_amenities import *
 """
 if app_views is not None:
     from api.v1.views.index import *
